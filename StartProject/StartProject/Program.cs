b0040ca3de71f2be9339a9c63d354ebf1c259779
RedGDS <redgds1819@gmail.com>
2026-06-06 22:44:17 +0500

    june 6 commit

--- StartProject/StartProject/Program.cs
+++ StartProject/StartProject/Program.cs
@@ -13,35 +13,42 @@
 
 //bool networkOk = true;
 //Console.WriteLine(networkOk);
-
-
-double width, height, woodLength, glassArea;
-const double MAX_WIDTH = 5.0;
-const double MIN_WIDTH = 0.5;
-const double MAX_HEIGHT = 3.0;
-const double MIN_HEIGHT = 0.75;
-string widthString, heightString;
-
-do
+using System;
+class Program
 {
-    Console.Write("Give the width of the window between " + MIN_WIDTH + " and " + MAX_WIDTH + ": ");
-    widthString = Console.ReadLine();
-    width = double.Parse(widthString);
-} while (width < MIN_WIDTH || width > MAX_WIDTH);
+    static void CalculateWindow()
+    {
+        double width, height, woodLength, glassArea;
+        const double MAX_WIDTH = 5.0;
+        const double MIN_WIDTH = 0.5;
+        const double MAX_HEIGHT = 3.0;
+        const double MIN_HEIGHT = 0.75;
+        string widthString, heightString;
 
-do
-{
-    Console.Write("Give the height of the window between " + MIN_HEIGHT + " and " + MAX_HEIGHT + ": ");
-    heightString = Console.ReadLine();
-    height = double.Parse(heightString);
-} while (height < MIN_HEIGHT || height > MAX_HEIGHT);
+        do
+        {
+            Console.Write("Give the width of the window between " + MIN_WIDTH + " and " + MAX_WIDTH + ": ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+        } while (width < MIN_WIDTH || width > MAX_WIDTH);
 
-woodLength = 2 * (width + height) * 3.25;
+        do
+        {
+            Console.Write("Give the height of the window between " + MIN_HEIGHT + " and " + MAX_HEIGHT + ": ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+        } while (height < MIN_HEIGHT || height > MAX_HEIGHT);
 
-glassArea = 2 * (width * height);
+        woodLength = 2 * (width + height) * 3.25;
 
-Console.WriteLine("The length of the wood is " + woodLength + " feet");
-Console.WriteLine("The area of the glass is " + glassArea + " square meters");
+        glassArea = 2 * (width * height);
 
-
+        Console.WriteLine("The length of the wood is " + woodLength + " feet");
+        Console.WriteLine("The area of the glass is " + glassArea + " square meters");
+    }
+    static void Main()
+    {
+        CalculateWindow();
+    }
+}
 
